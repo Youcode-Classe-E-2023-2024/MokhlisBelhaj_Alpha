be9501fd016f9de7d50b73d4a91c5555d6ff5d52
BelhajMokhlis <belhajmokhlis@gmail.com>
2024-01-07 22:27:34 +0100

    products.php delete

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -10,11 +10,5 @@ redirect('authentication/login');
     public function index(){
         $this->view('dashboard/index');
     }
-    public function post(){
-        $this->view('dashboard/users');
-    }
-    public function postsedite($id){
-        $data['Postid'] = $id;
-        $this->view('dashboard/products',$data);
-    }
+  
 }
